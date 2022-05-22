3c0645c05c5099fed31e79482b3d00ed25b26548
Zhenyu Shi <zhenyu.shi@compass.com>
2022-05-22 23:02:45 +0800

    Reformat configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,17 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
   stories: [
-    '../pages/**/*.stories.mdx',
-    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
-    '../components/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    "../pages/**/*.stories.mdx",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "staticDirs": ["../public"],
+  staticDirs: ["../public"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
   ],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
@@ -22,9 +22,9 @@ module.exports = {
     ];
     return config;
   },
-  framework: '@storybook/react',
+  framework: "@storybook/react",
   core: {
-    builder: 'webpack5',
+    builder: "webpack5",
   },
   // https://github.com/storybookjs/storybook/issues/10231
   features: {

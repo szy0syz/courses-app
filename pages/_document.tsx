9c0298895452b95a2c3cd3325bf3645284791e56
Zhenyu Shi <zhenyu.shi@compass.com>
2022-05-28 15:44:26 +0800

    Fix Layout and add Home Page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { Global } from "@emotion/react";
+import { GlobalStyles } from "@/styles/global";
+
 export default function Document() {
   return (
     <Html>
@@ -8,6 +11,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Poppins|Monoton&display=swap"
         />
+        <Global styles={GlobalStyles} />
       </Head>
       <body>
         <Main />
